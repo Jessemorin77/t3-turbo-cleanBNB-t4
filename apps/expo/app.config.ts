c38ce49ef2585cd581484039ebda348129dbc6ca
Jesse Morin <jessemorin77@gmail.com>
2023-05-03 22:22:38 -0400

    secound

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,6 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY = "pk_test_d2VhbHRoeS1hbGJhY29yZS01MS5jbGVyay5hY2NvdW50cy5kZXYk";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
